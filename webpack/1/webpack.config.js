78b34c1a8192f2ec252d38cad1da340ce75271a3
avmax <avmax.web@gmail.com>
2017-09-21 20:06:29 +0300

    webpack1: 2.8: Простая сборка, Минификация

--- webpack/1/webpack.config.js
+++ webpack/1/webpack.config.js
@@ -47,3 +47,15 @@ module.exports = {
         })
     ]
 };
+
+if (NODE_ENV === 'prod') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress : {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    )
+}
