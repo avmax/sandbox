7c5f84b4a68a89b4ee132eec83398f5197b40f28
avmax <avmax.web@gmail.com>
2017-09-22 01:56:32 +0300

    webpack1: 6.1: Стили и файлы, Компонент меню, style-loader

--- webpack/1/webpack.config.js
+++ webpack/1/webpack.config.js
@@ -6,22 +6,31 @@ const NODE_ENV = process.env.NODE_ENV || 'dev';
 
 module.exports = {
     context: __dirname + '/frontend',
-    entry: './app',
-
+    entry: './main',
     output: {
-        path: __dirname + '/public/js',
-        publicPath: '/',
-        filename: 'app.js',
+        path: __dirname + '/public',
+        filename: '[name].js'
     },
 
-    module : {
-        loaders: [{
-            test: /\.js$/,
-            include: __dirname + '/frontend',
-            loader: 'babel-loader'
-        }],
-
-        noParse: /node_modules\/(angular\/angular.js|jquery)/
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.jade$/,
+                loader: 'jade-loader'
+            },
+            {
+                test: /\.css/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
+        ]
     }
 };
 
