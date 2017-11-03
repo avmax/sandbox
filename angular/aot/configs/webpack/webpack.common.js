ed1c2d045c0d124a3be5110ea46297e92b9d6659
avmax <avmax.web@gmail.com>
2017-11-04 00:48:06 +0300

    aot: done

--- angular/aot/configs/webpack/webpack.common.js
+++ angular/aot/configs/webpack/webpack.common.js
@@ -55,14 +55,6 @@ module.exports = function(options) {
         },
 
         plugins: [
-            // Workaround for angular/angular#11580
-            new webpack.ContextReplacementPlugin(
-                // The (\\|\/) piece accounts for path separators in *nix and Windows
-                /angular(\\|\/)core(\\|\/)@angular/,
-                helpers.root('./src'), // location of your src
-                {} // a map of your routes
-            ),
-
             new webpack.optimize.CommonsChunkPlugin({
                 name: ['app', 'vendor', 'polyfills']
             }),
--- angular/aot/src/vendor.ts
+++ angular/aot/src/vendor.ts
@@ -1,6 +1,6 @@
 // Angular
 import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
+// import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/forms';
